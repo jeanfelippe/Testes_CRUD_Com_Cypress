4b892de015837cda31475726af63cc59435a392b
jean felippe <jeanfelippe_20@hotmail.com>
2024-04-02 23:06:07 -0300

    teste

---
+++ cypress/integration/CRUD Funcional/functional.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('Projeto Cypress',()=> {
+
+    before(()=>{
+        cy.visit('https://barrigareact.wcaquino.me/')
+    })
+
+    it('login',()=>{
+        cy.get('[data-test=email]').type('jeanfelippe500@gmail.com')
+        cy.get('[data-test=passwd]').type('123')
+        cy.get('.btn').click()
+        cy.get('.toast-message').should('Bem vindo, Jean 500')
+    })
+
+
+})
