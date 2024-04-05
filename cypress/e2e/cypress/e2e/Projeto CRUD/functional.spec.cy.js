2e45fa2c509d32a53e7e5cd22e0bb565430f58ad
jean felippe <jeanfelippe_20@hotmail.com>
2024-04-04 23:10:25 -0300

    Inserir feature Criar uma Conta

--- cypress/e2e/cypress/e2e/Projeto CRUD/functional.spec.cy.js
+++ cypress/e2e/cypress/e2e/Projeto CRUD/functional.spec.cy.js
@@ -4,14 +4,22 @@ describe('Projeto Cypress',()=> {
 
   before(()=>{
       cy.visit('https://barrigareact.wcaquino.me/')
-  })
-
-  it('login',()=>{
       cy.get('[data-test=email]').type('jeanfelippe500@gmail.com')
       cy.get('[data-test=passwd]').type('123')
       cy.get('.btn').click()
-   
+      //cy.get('.toast').should('contain','Bem Vindo')
   })
+
+  it('Criar uma conta',()=>{
+    cy.get('[data-test="menu-settings"]').click()
+    cy.get('[href="/contas"]').click()
+    cy.get('[data-test="nome"]').type('conta teste2')
+    cy.get('.btn').click()
+
+  
+  })
+
+
 
 
 })
